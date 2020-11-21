f8e5eb7d9901c4d1226e5cad12faea634390971a
Einar Budsted <ivazvaka@hotmail.com>
2020-11-21 02:54:10 +0100

    Some graphics and stuff

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool isSitting = false;
+    public Animator anim;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            isSitting = true;
+           
+
+        }
+    }
+}
